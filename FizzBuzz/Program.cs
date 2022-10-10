2d661d1e44548c9ad6cbdd56c520608bdbfc6e67
gemma <gemma@DESKTOP-2Q2G917>
2022-10-10 16:07:26 +0100

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Fizz;
+            int Buzz;
+            int FizzBuzz;
+
+            Random rng = new Random(100);
+
+            Fizz = 3;
+            Buzz = 5;
+            FizzBuzz = 3 + 5;
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
